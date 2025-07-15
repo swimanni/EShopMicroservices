7e9a770a302567c5b046e29e224cb7b037508ca6
Anirudh <swimanni@gmail.com>
2025-07-16 00:41:03 +0530

    Commit - Added Basket API

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.API.Basket.GetBasket;
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+public class GetBasketHandler(IBasketRepository repository)
+    : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //Get Basket from db
+        var basket = await repository.GetBasket(query.UserName);
+        return new GetBasketResult(basket);
+    }
+}

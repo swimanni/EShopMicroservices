bcf1bf5fbbbef7e8107231e279780036eb6c8b4f
Anirudh <swimanni@gmail.com>
2025-08-04 22:18:51 +0530

    Commit - Started working on Ordering microservice, completed Basked

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -31,6 +31,15 @@ builder.Services.AddStackExchangeRedisCache(options =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+}).ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
 });
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Domain.Models;
+
+public class Order : Aggregate<OrderId>
+{
+    private readonly List<OrderItem> __orderItems = new();
+    public IReadOnlyList<OrderItem> OrderItems => __orderItems.AsReadOnly();
+
+    public CustomerId CustomerId { get; private set; } = default!;
+    public OrderName OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
+    public Payment Payment { get; private set; } = default!;
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(x => x.Price * x.Quantity);
+        private set { }
+    }
+}
